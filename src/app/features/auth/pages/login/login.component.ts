a7aed8b154ccaa1a66d6e458e7ac0542e3ef9d34
Jose Mejia <jmejia.jm449@gmail.com>
2026-02-05 18:38:39 -0500

    feat: Se instala la libreria sweetalert2 y se implementa en el LoginComponent.

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, signal, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 import { AuthService } from '../../../../core/services/auth.service';
 
@@ -31,20 +32,40 @@ export class LoginComponent
       this.isLoading.set(true);
       const email = this.loginForm.value.email!;
       
-      this.authService.login(email).subscribe({
-          next: (response) => 
-          {
-            this.isLoading.set(false);
-            this.route.navigate(['/tasks']);
-          },
-          error: (err) =>
-          {
-            const { error } = err;
-            const messages = error.messages ?? 'Ha ocurrido un error inesperado.';
-            this.isLoading.set(false);
-            console.log(err);
-            alert(messages);
-          }
+      this.authService.login(email).subscribe(
+      {
+        next: (response) => 
+        {
+          this.isLoading.set(false);
+          Swal.fire({
+            title: 'Bienvenido',
+            text: response.messages,
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+            toast: true,           
+            position: 'top-end'
+          });
+          this.route.navigate(['/tasks']);
+        },
+        error: (err) =>
+        {
+          const { error } = err;
+          const messages = error.messages ?? 'Ha ocurrido un error inesperado.';
+          this.isLoading.set(false);
+          console.log(err);
+          Swal.fire({
+            icon: 'error',
+            title: 'Vaya...',
+            text: messages,
+            showConfirmButton: false,
+            timer: 3500,
+            timerProgressBar: true,
+            toast: true,           
+            position: 'top-end'
+          });
+        }
       });    
     }
   }
